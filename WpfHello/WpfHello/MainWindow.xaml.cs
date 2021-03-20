9e2a082065c2cd461a978c8a93507acb54b42fac
Alexander <alavrukhine@rixtrema.com>
2021-03-20 15:33:31 +0300

    labs

---
+++ WpfHello/WpfHello/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button.IsEnabled = false;
+            button1.IsEnabled = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamWriter sw = new
+                System.IO.StreamWriter("username.txt");
+                button1.IsEnabled = true;
+                sw.WriteLine(textBox.Text);
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr = new
+                System.IO.StreamReader("username.txt");
+                label.Content = "Приветствую Вас, уважаемый " + sr.ReadToEnd();
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            button.IsEnabled = true;
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            myTextBox.Text = DateTime.Now.ToString();
+        }
+    }
+}
